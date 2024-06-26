36c982f7642022bdc6617c0227520e8e7b02752c
ARUSHIGARG211004 <121043298+ARUSHIGARG211004@users.noreply.github.com>
2024-06-27 01:07:49 +0530

    Add database connection and User Schema

--- db/User.js
+++ db/User.js
@@ -1,9 +1,25 @@
-const mongoose =require('mongoose');
-const userSchema = new mongoose.Schema({
-    name:String,
-    email:String,
-    password:String
+const mongoose = require('mongoose');
 
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true 
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true, 
+        trim: true, 
+        lowercase: true 
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6 
+    }
 });
 
-module.exports= mongoose.model('users',userSchema);
+
+module.exports = mongoose.model('User', userSchema);
