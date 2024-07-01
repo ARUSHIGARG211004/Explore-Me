c3353b1928f7e3c281a2b6a7a10563ff761bf301
ARUSHIGARG211004 <121043298+ARUSHIGARG211004@users.noreply.github.com>
2024-07-01 13:53:34 +0530

    changes commited

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ require('./db/config');
 const User= require("./db/User");
 const signup= require("./signup");
 const login= require("./login");
+const collectionapi= require('./collection');
+const saveImageapi= require('./saveImage');
 
 const app = express();
 
@@ -15,5 +17,6 @@ app.use(express.json());
 
 app.use(signup);
 app.use(login);
-
+app.use(collectionapi);
+app.use(saveImageapi);
 app.listen(3000);
